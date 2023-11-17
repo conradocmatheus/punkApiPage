4f098162b0dd1b1ac7b6d1c8330c5671b4f5c34b
coradoo <conrado.matheus@protonmail.com>
2023-11-17 16:41:26 -0300

    commented js

--- script.js
+++ script.js
@@ -16,29 +16,34 @@ function getData() {
 				description: item.description,
 				image: item.image_url,
 			}));
-			totalPages = Math.ceil(beerList.length / itemsPerPage.value);
-			createList();
+			totalPages = Math.ceil(beerList.length / itemsPerPage.value); // Calcula o total de paginas
+			createList(); // Chama a funcao que cria a lista para exibir os dados
 		})
 		.catch((error) => {
+			// Caso ocorra algum erro, ele sera exibido no console
 			console.log("Error:", error);
 		});
 }
 
 itemsPerPage.addEventListener("change", () => {
-	totalPages = Math.ceil(beerList.length / itemsPerPage.value);
-	currentPage = 1;
-	createList();
+	// Evento que observa a mudanca do dropdown de itens por pagina
+	totalPages = Math.ceil(beerList.length / itemsPerPage.value); // Calcula o total de paginas
+	currentPage = 1; // Reseta a pagina atual
+	createList(); // Chama a funcao que cria a lista para atualizar a pagina
 });
 
 function createList() {
-	let start = (currentPage - 1) * itemsPerPage.value;
-	let end = start + parseInt(itemsPerPage.value);
-	let paginatedItems = beerList.slice(start, end);
+	// Funcao que cria a lista de acordo com a pagina atual e o numero de itens por pagina
+	let start = (currentPage - 1) * itemsPerPage.value; // Calcula o inicio da pagina atual de acordo com o numero de itens por pagina
+	let end = start + parseInt(itemsPerPage.value); // Calcula o fim da pagina atual de acordo com o numero de itens por pagina
+	let paginatedItems = beerList.slice(start, end); // Cria um novo array com os itens da pagina atual
 
-	let table = document.getElementById("beer-table");
-	table.innerHTML = "";
+	let table = document.getElementById("beer-table"); // Seleciona a tabela onde os dados serao exibidos
+	table.innerHTML = ""; // Limpa a tabela
 	paginatedItems.forEach((beer) => {
+		// Cria uma linha para cada item do array
 		let beerRow = document.createElement("tr");
+		// Adiciona os dados do item na linha
 		beerRow.innerHTML = `
             <td><img src="${beer.image}" alt="${beer.name}" width="100px"></td>
             <td>${beer.id}</td>
@@ -46,20 +51,22 @@ function createList() {
             <td>${beer.tagline}</td>
             <td>${beer.description}</td>
         `;
-		table.appendChild(beerRow);
+		table.appendChild(beerRow); // Adiciona a linha na tabela no HTML
 	});
 
-	let paginationControls = document.getElementById("paginationControls");
-	paginationControls.innerHTML = "";
+	let paginationControls = document.getElementById("paginationControls"); // Seleciona o elemento onde os botoes de paginacao serao exibidos
+	paginationControls.innerHTML = ""; // Limpa o elemento
 	for (let i = 1; i <= totalPages; i++) {
-		let button = document.createElement("button");
-		button.innerText = i;
+		// For que cria os botoes de paginacao
+		let button = document.createElement("button"); // Cria o botao
+		button.innerText = i; // Adiciona o numero da pagina no botao
 		button.addEventListener("click", () => {
-			currentPage = i;
-			createList();
+			// Arrow function que observa quando o botao e clicado
+			currentPage = i; // Atribui o valor do botao a pagina atual
+			createList(); // Chama a funcao que cria a lista para atualizar a pagina
 		});
-		paginationControls.appendChild(button);
+		paginationControls.appendChild(button); // Adiciona o botao no HTML
 	}
 }
 
-getData();
+getData(); // Chama a funcao que chama a API
